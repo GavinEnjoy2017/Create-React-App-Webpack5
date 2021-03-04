ec6ccd0d94c8d21aaa3e5157ed848de2b6270235
GavinEnjoy2017 <974775793@qq.com>
2021-03-05 00:43:18 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./style.scss";
+
+const App = () => {
+    return (
+        <div>
+            App入口
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
